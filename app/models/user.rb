ed1f12d3de685575f38d91e3a7e7b213d98871ae
Murarius <murarius2011@gmail.com>
2016-04-27 13:23:54 +0200

    add basic validations to Product and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :rememberable, :trackable,
   devise :database_authenticatable, :registerable, :recoverable, :validatable
+
+  validates :email, presence: true
+  validates :password, presence: true
 end
